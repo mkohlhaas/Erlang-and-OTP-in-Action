cc51cbb330747d3437ec5768e3b4b8a99664b2c1
Hans-J. Schmid <knock@myopendoor.de>
2023-03-02 19:12:11 +0100

    reorganizes directory structure.

---
+++ README.md
@@ -0,0 +1,3 @@
+![Erlang and OTP in Action](https://images.manning.com/360/480/resize/book/f/d21069b-7a05-4be7-bcd8-7d38aff03e34/logan.png "Erlang and OTP in Action")
+
+This repository contains the source code for the book [Erlang and OTP in Action](https://www.manning.com/books/erlang-and-otp-in-action), by Martin Logan, Eric Merritt, and Richard Carlsson, Manning Publications, 2010.
--- misc/profile_ex.erl
+++ misc/profile_ex.erl
@@ -19,37 +19,36 @@
 %% @end
 %%--------------------------------------------------------------------
 run_with_fprof() ->
-    fprof:trace(start),
-    run(),
-    timer:sleep(5000),
-    fprof:trace(stop),
-    fprof:profile(),
-    fprof:analyse({dest, atom_to_list(?MODULE)}).
-    
-    
+  fprof:trace(start),
+  run(),
+  timer:sleep(5000),
+  fprof:trace(stop),
+  fprof:profile(),
+  fprof:analyse({dest, atom_to_list(?MODULE)}).
+
 %%--------------------------------------------------------------------
 %% @doc start two nearly identical running processes
 %% @spec () -> ok
 %% @end
 %%--------------------------------------------------------------------
 run() ->
-    spawn(fun() -> looper(1000) end),
-    spawn(fun() -> funner(1000) end).
+  spawn(fun() -> looper(1000) end),
+  spawn(fun() -> funner(1000) end).
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 looper(0) ->
-    ok;
+  ok;
 looper(N) ->
-    integer_to_list(N),
-    looper(N - 1).
+  integer_to_list(N),
+  looper(N - 1).
 
 funner(N) ->
-    funner(fun(N_) -> integer_to_list(N_) end, N).
+  funner(fun(N_) -> integer_to_list(N_) end, N).
 
 funner(_Fun, 0) ->
-    ok;
+  ok;
 funner(Fun, N) ->
-    Fun(N),
-    funner(Fun, N - 1).
+  Fun(N),
+  funner(Fun, N - 1).
