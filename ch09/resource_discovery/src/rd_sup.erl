9cc65fca2751ba6af26293b670169a077ced0ddf
Hans-J. Schmid <knock@myopendoor.de>
2023-03-07 02:20:10 +0100

    reformats ch. 10.

--- ch09/resource_discovery/src/rd_sup.erl
+++ ch09/resource_discovery/src/rd_sup.erl
@@ -2,20 +2,24 @@
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
-
-%% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
+%%%%%%%
+% API %
+%%%%%%%
+
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%%%%%%%%%%%
+% Callbacks %
+%%%%%%%%%%%%%
 
 init([]) ->
-    Server = {rd_server, {rd_server, start_link, []},
-              permanent, 2000, worker, [rd_server]},
-    Children = [Server],
-    RestartStrategy = {one_for_one, 0, 1},
-    {ok, {RestartStrategy, Children}}.
+  Server = {rd_server, {rd_server, start_link, []}, permanent, 2000, worker, [rd_server]},
+  Children = [Server],
+  RestartStrategy = {one_for_one, 0, 1},
+  {ok, {RestartStrategy, Children}}.
--- ch09/simple_cache/src/sc_sup.erl
+++ ch09/simple_cache/src/sc_sup.erl
@@ -2,24 +2,34 @@
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
-
-%% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
+%%%%%%%
+% API %
+%%%%%%%
+
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%%%%%%%%%%%
+% Callbacks %
+%%%%%%%%%%%%%
 
 init([]) ->
-    ElementSup = {sc_element_sup, {sc_element_sup, start_link, []},
-                  permanent, 2000, supervisor, [sc_element]},
+  ElementSup =
+    {sc_element_sup,
+     {sc_element_sup, start_link, []},
+     permanent,
+     2000,
+     supervisor,
+     [sc_element]},
 
-    EventManager = {sc_event, {sc_event, start_link, []},
-                    permanent, 2000, worker, [sc_event]},
+  EventManager =
+    {sc_event, {sc_event, start_link, []}, permanent, 2000, worker, [sc_event]},
 
-    Children = [ElementSup, EventManager],
-    RestartStrategy = {one_for_one, 4, 3600},
-    {ok, {RestartStrategy, Children}}.
+  Children = [ElementSup, EventManager],
+  RestartStrategy = {one_for_one, 4, 3600},
+  {ok, {RestartStrategy, Children}}.
