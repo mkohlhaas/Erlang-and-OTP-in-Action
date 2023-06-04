914913f430e1a2c087794cb472c38fa62eba0038
Hans-J. Schmid <knock@myopendoor.de>
2023-06-04 21:37:09 +0200

    adds rebar projects for ch03 and ch04.

--- rebar/ch04/tr_server/src/tr_server.erl
+++ rebar/ch04/tr_server/src/tr_server.erl
@@ -13,14 +13,8 @@
 -behaviour(gen_server).
 
 -export([start_link/1, start_link/0, get_count/0, stop/0]).
--export([
-    init/1,
-    handle_call/3,
-    handle_cast/2,
-    handle_info/2,
-    terminate/2,
-    code_change/3
-]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
 
 -define(SERVER, ?MODULE).
 -define(DEFAULT_PORT, 1055).
