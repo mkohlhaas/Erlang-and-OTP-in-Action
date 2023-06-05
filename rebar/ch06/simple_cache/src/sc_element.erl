55cd98dd248b009a3c0cbba6d8024fdc5219693c
Hans-J. Schmid <knock@myopendoor.de>
2023-06-05 02:29:04 +0200

    adds rebar project for ch. 6.

--- rebar/ch06/simple_cache/src/sc_element.erl
+++ rebar/ch06/simple_cache/src/sc_element.erl
@@ -1,19 +1,11 @@
 % For every key there is a separate process (a simple cache element = sc_element) which stores the value!
 % Implemented as gen_server. Is used as a dynamic child for the supervisor.
-
 -module(sc_element).
 
 -behaviour(gen_server).
 
 -export([start_link/2, create/2, create/1, fetch/1, replace/2, delete/1]).
--export([
-    init/1,
-    handle_call/3,
-    handle_cast/2,
-    handle_info/2,
-    terminate/2,
-    code_change/3
-]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(DEFAULT_LEASE_TIME, 60).
 
@@ -23,23 +15,17 @@
 % API %
 %%%%%%%
 
-start_link(Value, LeaseTime) ->
-    gen_server:start_link(?MODULE, [Value, LeaseTime], []).
+start_link(Value, LeaseTime) -> gen_server:start_link(?MODULE, [Value, LeaseTime], []).
 
-create(Value, LeaseTime) ->
-    sc_sup:start_child(Value, LeaseTime).
+create(Value, LeaseTime) -> sc_sup:start_child(Value, LeaseTime).
 
-create(Value) ->
-    create(Value, ?DEFAULT_LEASE_TIME).
+create(Value) -> create(Value, ?DEFAULT_LEASE_TIME).
 
-fetch(Pid) ->
-    gen_server:call(Pid, fetch).
+fetch(Pid) -> gen_server:call(Pid, fetch).
 
-replace(Pid, Value) ->
-    gen_server:cast(Pid, {replace, Value}).
+replace(Pid, Value) -> gen_server:cast(Pid, {replace, Value}).
 
-delete(Pid) ->
-    gen_server:cast(Pid, delete).
+delete(Pid) -> gen_server:cast(Pid, delete).
 
 %%%%%%%%%%%%%
 % Callbacks %
@@ -47,12 +33,14 @@ delete(Pid) ->
 
 % Actually not a good idea to use time-outs.
 % Will bog down the server when you have many processes.
-
 init([Value, LeaseTime]) ->
     Now = calendar:local_time(),
     StartTime = calendar:datetime_to_gregorian_seconds(Now),
-    {ok, #state{value = Value, lease_time = LeaseTime, start_time = StartTime},
-        time_left(StartTime, LeaseTime)}.
+    {
+        ok,
+        #state{value = Value, lease_time = LeaseTime, start_time = StartTime},
+        time_left(StartTime, LeaseTime)
+    }.
 
 handle_call(fetch, _From, State) ->
     #state{value = Value, lease_time = LeaseTime, start_time = StartTime} = State,
@@ -66,15 +54,13 @@ handle_cast({replace, Value}, State) ->
 handle_cast(delete, State) ->
     {stop, normal, State}.
 
-handle_info(timeout, State) ->
-    {stop, normal, State}.
+handle_info(timeout, State) -> {stop, normal, State}.
 
 terminate(_Reason, _State) ->
     sc_store:delete(self()),
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 %%%%%%%%%%%%%%%%%%%%%%
 % Internal Functions %
@@ -84,7 +70,6 @@ code_change(_OldVsn, State, _Extra) ->
 % If you forget to return a new timeout value in one of the call-back functions,
 % the timeout will revert to infinity. When you're using server timeouts,
 % it's important to remember to set them in EVERY clause of EVERY call-back function.
-
 time_left(_StartTime, infinity) ->
     infinity;
 time_left(StartTime, LeaseTime) ->
@@ -92,8 +77,6 @@ time_left(StartTime, LeaseTime) ->
     CurrentTime = calendar:datetime_to_gregorian_seconds(Now),
     TimeElapsed = CurrentTime - StartTime,
     case LeaseTime - TimeElapsed of
-        Time when Time =< 0 ->
-            0;
-        Time ->
-            Time * 1000
+        Time when Time =< 0 -> 0;
+        Time -> Time * 1000
     end.
--- rebar/ch06/simple_cache/src/sc_store.erl
+++ rebar/ch06/simple_cache/src/sc_store.erl
@@ -1,6 +1,5 @@
 % Store for (key -> pid) mappings; basically a wrapper for ETS (Erlang Term Storage).
 % Every process (pid of gen_server) stores the value.
-
 -module(sc_store).
 
 -export([init/0, insert/2, delete/1, lookup/1]).
@@ -11,16 +10,12 @@ init() ->
     ets:new(?TABLE_ID, [public, named_table]),
     ok.
 
-insert(Key, Pid) ->
-    ets:insert(?TABLE_ID, {Key, Pid}).
+insert(Key, Pid) -> ets:insert(?TABLE_ID, {Key, Pid}).
 
 lookup(Key) ->
     case ets:lookup(?TABLE_ID, Key) of
-        [{Key, Pid}] ->
-            {ok, Pid};
-        [] ->
-            {error, not_found}
+        [{Key, Pid}] -> {ok, Pid};
+        [] -> {error, not_found}
     end.
 
-delete(Pid) ->
-    ets:match_delete(?TABLE_ID, {'_', Pid}).
+delete(Pid) -> ets:match_delete(?TABLE_ID, {'_', Pid}).
