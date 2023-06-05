9e667966a7cbc545303ed544f1bd1a63bda69ad2
Hans-J. Schmid <knock@myopendoor.de>
2023-06-06 00:26:38 +0200

    adds comments and rebar project for event handling and logging.

--- ch07/die_please/die_please1.erl
+++ ch07/die_please/die_please1.erl
@@ -2,10 +2,6 @@
 
 -export([go/0]).
 
--define(SLEEP_TIME, 2000).
-
 go() ->
-    %% just sleep for a while, then crash
-    timer:sleep(?SLEEP_TIME),
     % matching will fail and cause an exception (on purpose)
     i_really_want_to_die = right_now.
