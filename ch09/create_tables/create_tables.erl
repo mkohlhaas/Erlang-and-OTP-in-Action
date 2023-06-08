5f863408f21d7cbec41548e264e52c96dbd7995d
Hans-J. Schmid <knock@myopendoor.de>
2023-06-08 23:05:33 +0200

    creates a rebar3 project for ch.9.

--- ch09/create_tables/create_tables.erl
+++ ch09/create_tables/create_tables.erl
@@ -5,30 +5,25 @@
 -include("create_tables.hrl").
 
 init_tables() ->
-    mnesia:create_table(user, [{attributes, record_info(fields, user)}]),
-    mnesia:create_table(project, [{attributes, record_info(fields, project)}]),
-    mnesia:create_table(
-        contributor,
-        [{type, bag}, {attributes, record_info(fields, contributor)}]
-    ).
+  mnesia:create_table(user, [{attributes, record_info(fields, user)}]),
+  mnesia:create_table(project, [{attributes, record_info(fields, project)}]),
+  mnesia:create_table(contributor, [{type, bag}, {attributes, record_info(fields, contributor)}]).
 
 insert_project(Title, Description) ->
-    mnesia:dirty_write(#project{title = Title, description = Description}).
+  mnesia:dirty_write(#project{title = Title, description = Description}).
 
 insert_user(Id, Name, ProjectTitles) when ProjectTitles =/= [] ->
-    User = #user{id = Id, name = Name},
-    Fun =
-        fun() ->
-            % insert user
-            mnesia:write(User),
-            lists:foreach(
-                fun(Title) ->
-                    % make sure project exists
-                    [#project{title = Title}] = mnesia:read(project, Title),
-                    % connect user and project (m:m relationship)
-                    mnesia:write(#contributor{user_id = Id, project_title = Title})
-                end,
-                ProjectTitles
-            )
-        end,
-    mnesia:transaction(Fun).
+  User = #user{id = Id, name = Name},
+  Fun =
+    fun() ->
+       % insert user
+       mnesia:write(User),
+       lists:foreach(fun(Title) ->
+                        % make sure project exists
+                        [#project{title = Title}] = mnesia:read(project, Title),
+                        % connect user and project (m:m relationship)
+                        mnesia:write(#contributor{user_id = Id, project_title = Title})
+                     end,
+                     ProjectTitles)
+    end,
+  mnesia:transaction(Fun).
